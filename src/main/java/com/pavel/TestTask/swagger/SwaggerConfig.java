9eb1a0bc835b51f987e172e74a146e222e4677e1
pavek <pavek@172.16.28.13>
2019-01-26 23:00:27 +0200

    fix

--- src/main/java/com/pavel/TestTask/swagger/SwaggerConfig.java
+++ src/main/java/com/pavel/TestTask/swagger/SwaggerConfig.java
@@ -1,17 +1,23 @@
 package com.pavel.TestTask.swagger;
 
-import static com.google.common.base.Predicates.or;
-import static springfox.documentation.builders.PathSelectors.regex;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.base.Predicate;
+import com.google.common.collect.Lists;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -19,21 +25,31 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
 	@Bean
-	public Docket postsApi() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
-				.apiInfo(apiInfo()).select().paths(postPaths()).build();
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build().securitySchemes(Arrays.asList(apiKey()))
+				.securitySchemes(Lists.newArrayList(apiKey())).securityContexts(Arrays.asList(securityContext()));
 	}
 
-	private Predicate<String> postPaths() {
-		return or(regex("/api/posts.*"), regex("/auth/javainuse.*"));
+	private ApiKey apiKey() {
+		return new ApiKey("apiKey", "Authorization", "header");
 	}
 
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Test for Java Junior API")
-				.description("API reference for developers")
-				.termsOfServiceUrl("http://javainuse.com")
-				.contact("javainuse@gmail.com").license("JavaInUse License")
-				.licenseUrl("javainuse@gmail.com").version("1.0").build();
+	@Bean
+	public SecurityConfiguration security() {
+		return SecurityConfigurationBuilder.builder().scopeSeparator(",").additionalQueryStringParams(null)
+				.useBasicAuthenticationWithAccessCodeGrant(false).build();
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("apiKey", authorizationScopes));
 	}
 
 }
